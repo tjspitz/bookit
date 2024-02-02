10f651bca3f2286b5e7b4a943831b2db69eea093
Tom Spitz <tjspitz1986@gmail.com>
2024-02-02 13:33:50 -0600

    add javadoc

--- server/src/main/java/com/mars/bookit/springboot/BookitApplication.java
+++ server/src/main/java/com/mars/bookit/springboot/BookitApplication.java
@@ -3,9 +3,15 @@ package com.mars.bookit.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author tjspitz
+ */
 @SpringBootApplication
 public class BookitApplication {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BookitApplication.class, args);
 	}
