c71024c893c6db901a25803abee57eacbbc20af9
Tom Spitz <tjspitz1986@gmail.com>
2024-02-02 22:46:01 -0600

    implement CORS handling

--- server/src/main/java/com/mars/bookit/springboot/BookitApplication.java
+++ server/src/main/java/com/mars/bookit/springboot/BookitApplication.java
@@ -2,6 +2,9 @@ package com.mars.bookit.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author tjspitz
@@ -9,11 +12,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookitApplication {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(BookitApplication.class, args);
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(BookitApplication.class, args);
+    }
 
+    /**
+     * @return WebMvcConfigurer
+     */
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
