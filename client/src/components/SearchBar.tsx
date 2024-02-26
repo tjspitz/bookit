c9eb08a516e82f57bf421b28f237f8703ce41c6d
Tom Spitz <tjspitz1986@gmail.com>
2024-02-26 12:35:39 -0600

    a very basic easter egg, just because...

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,6 +1,10 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, KeyboardEvent, SetStateAction, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
+import surprise from '../assets/secret.gif';
+
+const theSecret =
+  'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightBA';
 
 const SearchBar = ({
   pnr,
@@ -9,7 +13,21 @@ const SearchBar = ({
   pnr: string;
   setPnr: Dispatch<SetStateAction<string>>;
 }) => {
+  const [secret, setSecret] = useState<string>('');
   const navigate = useNavigate();
+
+  const handleSecret = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (
+      e.key === 'ArrowUp' ||
+      e.key === 'ArrowDown' ||
+      e.key === 'ArrowLeft' ||
+      e.key === 'ArrowRight' ||
+      (secret === theSecret.slice(0, -2) && e.key === 'B') ||
+      (secret === theSecret.slice(0, -1) && e.key === 'A')
+    ) {
+      setSecret(secret + e.key);
+    }
+  };
 
   return (
     <>
@@ -19,11 +37,17 @@ const SearchBar = ({
         placeholder="Passenger/Ticket No."
         value={pnr}
         onChange={(e) => setPnr(e.target.value.toUpperCase())}
+        onKeyDown={handleSecret}
       />
       <Button
         type="button"
         variant="dark"
-        onClick={() => navigate(`/tickets/${pnr}`)}
+        onClick={() => {
+          if (secret === theSecret) {
+            window.open(surprise, '_blank');
+          }
+          navigate(`/tickets/${pnr}`);
+        }}
       >
         Search
       </Button>
