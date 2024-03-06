ee5fe9fab56dfe80a01fb60ee1a3faa3245d36df
Tom Spitz <tjspitz1986@gmail.com>
2024-03-05 20:51:41 -0600

    v2 first commit wipe it clean, start w/ new template

--- client/src/components/LoginDemo.tsx
+++ client/src/components/LoginDemo.tsx
@@ -1,59 +1,16 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import '../styles/LoginDemo.css';
 
-import { useState } from 'react';
-import SelfDestruct from './sub_LoginDemo/SelfDestruct';
-import UpperLeftPics from './sub_LoginDemo/UpperLeftPics';
-import UpperLeftQuotes from './sub_LoginDemo/UpperLeftQuotes';
-import UpperRightFooter from './sub_LoginDemo/UpperRightFooter';
-import UpperRightForm from './sub_LoginDemo/UpperRightForm';
-import UpperRightLogos from './sub_LoginDemo/UpperRightLogos';
-
 const LoginDemo = () => {
-  const [modalShow, setModalShow] = useState<boolean>(false);
 
   return (
-    <Container>
-      {/* uppper section */}
-      <Row className="login-upper">
-        {/* uppper left quadrant */}
-        <Col
-          className="login-upper-left"
-          xs={12}
-          md={7}
-        >
-          <Row className="p-4">
-            <UpperLeftPics />
-          </Row>
-          <Row className="p-4">
-            <UpperLeftQuotes />
-          </Row>
-        </Col>
-        {/* uppper right quadrant */}
-        <Col
-          className="login-upper-right"
-          xs={12}
-          md={5}
-        >
-          <UpperRightLogos />
-          <div className=" mb-5 fake-border"></div>
-          <UpperRightForm
-            modalShow={modalShow}
-            setModalShow={setModalShow}
-          />
-          <UpperRightFooter />
-        </Col>
-      </Row>
-      {/* lower section (image banner) */}
-      <Row
-        className="login-lower"
-        fluid
-      />
-      <SelfDestruct
-        modalShow={modalShow}
-        setModalShow={setModalShow}
-      />
-    </Container>
+    <main className="background">
+      <Container className=" border border-dark rounded p-5 parent-container">
+        <Container className="border border-secondary rounded p-4 child-container">
+          <p>This is the child container</p>
+        </Container>
+      </Container>
+    </main>
   );
 };
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
-import Booking from './components/Booking';
-import Ticket from './components/Ticket';
 import LoginDemo from './components/LoginDemo';
-import { getTicketByPnr } from './lib/api';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles/index.css';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/booking',
-        element: <Booking />,
-      },
-      {
-        path: '/tickets/:pnr',
-        element: <Ticket />,
-        loader: async ({ params }) => {
-          const ticket = await getTicketByPnr(params?.pnr || '');
-          return ticket ? ticket : params?.pnr || '';
-        },
-      },
-    ],
-  },
   {
     path: '/login-demo',
     element: <LoginDemo />,
